1d2d3730d34fd925093cbc51f367d05f78e440c8
Pascal Post <pp-post@mailbox.org>
2023-04-07 23:27:23 +0200

    restructuration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod todo;
 
-use crate::todo::{Todo, TodoEntry, TodoStatus};
-use axum::{extract::State, response::IntoResponse, routing, Json, Router};
-use hyper::StatusCode;
+use crate::todo::{create_todo, list_todos, Todo, TodoEntry, TodoStatus};
+use axum::{routing, Router};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use utoipa::OpenApi;
@@ -15,7 +14,7 @@ async fn main() -> Result<(), hyper::Error> {
 
     #[derive(OpenApi)]
     #[openapi(
-        paths(list_todos, create_todo),
+        paths(todo::list_todos, todo::create_todo),
         components(schemas(Todo, TodoEntry, TodoStatus)),
         tags((name = "todo", description = "Todo items management API"))
     )]
@@ -30,39 +29,4 @@ async fn main() -> Result<(), hyper::Error> {
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
-}
-
-/// list all todos
-#[utoipa::path(
-    get,
-    path = "/todo",
-    responses(
-        (status = 200, description = "List all todos successfully", body = [TodoEntry])
-    )
-)]
-async fn list_todos(State(todos): State<Arc<Mutex<Vec<TodoEntry>>>>) -> Json<Vec<TodoEntry>> {
-    let todos = todos.lock().await.clone();
-    Json(todos)
-}
-
-/// create a new todo
-#[utoipa::path(
-    post,
-    path = "/todo",
-    request_body = Todo,
-    responses(
-        (status = 201, description = "Todo item created successfully", body = TodoEntry),
-    )
-)]
-async fn create_todo(
-    State(todos): State<Arc<Mutex<Vec<TodoEntry>>>>,
-    Json(todo): Json<Todo>,
-) -> impl IntoResponse {
-    let mut todos = todos.lock().await;
-    let id = todos.len() as i32;
-    let todo_entry = TodoEntry::new(id, todo);
-
-    todos.push(todo_entry.clone());
-
-    (StatusCode::CREATED, Json(todo_entry)).into_response()
 }
--- src/todo.rs
+++ src/todo.rs
@@ -1,4 +1,9 @@
+use std::sync::Arc;
+
+use axum::{extract::State, response::IntoResponse, Json};
+use hyper::StatusCode;
 use serde::{Deserialize, Serialize};
+use tokio::sync::Mutex;
 use utoipa::ToSchema;
 
 /// todo item
@@ -33,4 +38,39 @@ impl TodoEntry {
             status: TodoStatus::Todo,
         }
     }
+}
+
+/// list all todos
+#[utoipa::path(
+  get,
+  path = "/todo",
+  responses(
+      (status = 200, description = "List all todos successfully", body = [TodoEntry])
+  )
+)]
+pub async fn list_todos(State(todos): State<Arc<Mutex<Vec<TodoEntry>>>>) -> Json<Vec<TodoEntry>> {
+    let todos = todos.lock().await.clone();
+    Json(todos)
+}
+
+/// create a new todo
+#[utoipa::path(
+  post,
+  path = "/todo",
+  request_body = Todo,
+  responses(
+      (status = 201, description = "Todo item created successfully", body = TodoEntry),
+  )
+)]
+pub async fn create_todo(
+    State(todos): State<Arc<Mutex<Vec<TodoEntry>>>>,
+    Json(todo): Json<Todo>,
+) -> impl IntoResponse {
+    let mut todos = todos.lock().await;
+    let id = todos.len() as i32;
+    let todo_entry = TodoEntry::new(id, todo);
+
+    todos.push(todo_entry.clone());
+
+    (StatusCode::CREATED, Json(todo_entry)).into_response()
 }
