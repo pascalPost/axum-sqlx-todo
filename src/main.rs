921b716a6d6dfe6c25e0f44cb9900b78b9b4b92e
Pascal Post <pp-post@mailbox.org>
2023-04-07 21:30:31 +0200

    initial setup

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use axum::{routing::get, Router};
+use utoipa::{
+    openapi::security::{ApiKey, ApiKeyValue, SecurityScheme},
+    Modify, OpenApi,
+};
+use utoipa_swagger_ui::SwaggerUi;
+
+#[tokio::main]
+async fn main() -> Result<(), hyper::Error> {
+    #[derive(OpenApi)]
+    #[openapi()]
+    struct ApiDoc;
+
+    // build our application with a single route
+    let app = Router::new()
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
+        .route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+}
