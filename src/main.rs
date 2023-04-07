14125631845683aa640dc92fe3b4eea6c3898fb3
Pascal Post <pp-post@mailbox.org>
2023-04-07 23:19:47 +0200

    Addition of first post function

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
+mod todo;
+
+use crate::todo::{Todo, TodoEntry, TodoStatus};
+use axum::{extract::State, response::IntoResponse, routing, Json, Router};
+use hyper::StatusCode;
 use std::sync::Arc;
 use tokio::sync::Mutex;
-
-use axum::{extract::State, routing, Json, Router};
-use utoipa::{
-    openapi::security::{ApiKey, ApiKeyValue, SecurityScheme},
-    Modify, OpenApi,
-};
+use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
-
-mod todo;
-use crate::todo::Todo;
 
 #[tokio::main]
 async fn main() -> Result<(), hyper::Error> {
     // temporary todo in memory storage
-    let mut storage = Mutex::new(Vec::<Todo>::new());
+    let storage = Mutex::new(Vec::<TodoEntry>::new());
 
     #[derive(OpenApi)]
-    #[openapi(paths(list_todos), components(schemas(Todo)))]
+    #[openapi(
+        paths(list_todos, create_todo),
+        components(schemas(Todo, TodoEntry, TodoStatus))
+    )]
     struct ApiDoc;
 
     let app = Router::new()
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
-        .route("/todo", routing::get(list_todos))
-        .with_state(Arc::new(storage))
-        .route("/", routing::get(|| async { "Hello, World!" }));
+        .route("/todo", routing::get(list_todos).post(create_todo))
+        .with_state(Arc::new(storage));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -37,10 +36,32 @@ async fn main() -> Result<(), hyper::Error> {
     get,
     path = "/todo",
     responses(
-        (status = 200, description = "List all todos successfully", body = [Todo])
+        (status = 200, description = "List all todos successfully", body = [TodoEntry])
     )
 )]
-async fn list_todos(State(todos): State<Arc<Mutex<Vec<Todo>>>>) -> Json<Vec<Todo>> {
+async fn list_todos(State(todos): State<Arc<Mutex<Vec<TodoEntry>>>>) -> Json<Vec<TodoEntry>> {
     let todos = todos.lock().await.clone();
     Json(todos)
+}
+
+/// create a new todo
+#[utoipa::path(
+    post,
+    path = "/todo",
+    request_body = Todo,
+    responses(
+        (status = 201, description = "Todo item created successfully", body = TodoEntry),
+    )
+)]
+async fn create_todo(
+    State(todos): State<Arc<Mutex<Vec<TodoEntry>>>>,
+    Json(todo): Json<Todo>,
+) -> impl IntoResponse {
+    let mut todos = todos.lock().await;
+    let id = todos.len() as i32;
+    let todo_entry = TodoEntry::new(id, todo);
+
+    todos.push(todo_entry.clone());
+
+    (StatusCode::CREATED, Json(todo_entry)).into_response()
 }
--- src/todo.rs
+++ src/todo.rs
@@ -1,10 +1,36 @@
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
+/// todo item
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub struct Todo {
-    id: i32,
     #[schema(example = "Buy groceries")]
     value: String,
-    done: bool,
+}
+
+/// todo status enum
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, ToSchema)]
+pub enum TodoStatus {
+    Todo,
+    InProgress,
+    OnHold,
+    Done,
+}
+
+/// todo database entry
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+pub struct TodoEntry {
+    id: i32,
+    todo: Todo,
+    status: TodoStatus,
+}
+
+impl TodoEntry {
+    pub fn new(id: i32, todo: Todo) -> Self {
+        Self {
+            id,
+            todo,
+            status: TodoStatus::Todo,
+        }
+    }
 }
