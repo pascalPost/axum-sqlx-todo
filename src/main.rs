28759ba375010f5581209e4abadd3b7b627c0055
Pascal Post <pascal-post@mailbox.org>
2023-04-25 00:42:34 +0200

    random port

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use rust_rest_api::app;
 use std::error::Error;
+use std::net::TcpListener;
 
 #[derive(Clone)]
 struct AppState {}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let addr = listener.local_addr().unwrap();
+    tracing::debug!("listening on {}", &addr);
+
+    println!("Webserver listening on {}", &addr);
+
     let app = app().await?;
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::from_tcp(listener)?
         .serve(app.into_make_service())
         .await?;
     Ok(())
